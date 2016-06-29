a91053f7e1b11264ebbf035a9829ac44ca8bd136
Andrew Head <head.andrewm@gmail.com>
2016-06-29 10:22:43 -0700

    Dump: Include concern index in confidence ratings dump

--- dump/confidence_ratings.py
+++ dump/confidence_ratings.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 import logging
 
+from compute.task_periods import _get_concern_index
 from dump import dump_csv
 from models import Question
 
@@ -11,13 +12,15 @@ from models import Question
 logger = logging.getLogger('data')
 
 
-@dump_csv(__name__, ["User", "Question Index", "Concern", "Confidence"])
+@dump_csv(__name__, ["User", "Question Index", "Concern Index", "Concern", "Confidence"])
 def main(*args, **kwargs):
 
     for question in Question.select():
+        concern_index = _get_concern_index(question.user_id, question.question_index)
         yield [[
             question.user_id,
             question.question_index,
+            concern_index,
             question.concern,
             question.likert_confidence,
         ]]

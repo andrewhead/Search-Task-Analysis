5a22fb46317ad37eeff85c8f60adcfb5013b65f0
Andrew Head <head.andrewm@gmail.com>
2016-06-30 09:13:05 -0700

    Compute: Task periods measure from start of task to end of responses

--- compute/task_periods.py
+++ compute/task_periods.py
@@ -49,7 +49,7 @@ def compute_task_periods():
             if question_event.event_type == 'get task':
                 start_task_event = question_event
 
-            elif question_event.event_type == 'post task':
+            elif question_event.event_type == 'post responses':
 
                 if start_task_event is not None:
 

a980ad85fff65dc419abb25ad0a2a9f3d892f68b
Andrew Head <head.andrewm@gmail.com>
2016-06-29 22:00:42 -0700

    Dump: Only dump latest computed location visits and ratings

--- dump/location_ratings.py
+++ dump/location_ratings.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import logging
+from peewee import fn
 
 from dump import dump_csv
 from _urls import get_domain_name
@@ -17,7 +18,17 @@ logger = logging.getLogger('data')
     "URL", "Domain", "Rating", "Page Title", "Visit Date"])
 def main(*args, **kwargs):
 
-    for rating in LocationRating.select():
+    # Only dump the most recently computed location ratings (ignore all others).
+    latest_compute_index = LocationRating.select(fn.Max(LocationRating.compute_index)).scalar()
+    ratings = (
+        LocationRating
+        .select()
+        .where(
+            LocationRating.compute_index == latest_compute_index
+        )
+    )
+
+    for rating in ratings:
         yield [[
             rating.compute_index,
             rating.user_id,
@@ -34,4 +45,4 @@ def main(*args, **kwargs):
 
 
 def configure_parser(parser):
-    parser.description = "Dump records of all user ratings of web pages."
+    parser.description = "Dump most recently computed record of all user ratings of web pages."
--- dump/location_visits.py
+++ dump/location_visits.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import logging
+from peewee import fn
 
 from dump import dump_csv
 from _urls import get_domain_name
@@ -30,8 +31,17 @@ DOMAINS = [
     "URL", "Domain", "Page Title", "Start Time", "End Time"])
 def main(*args, **kwargs):
 
-    for visit in LocationVisit.select():
+    # Only dump the most recently computed location visits (ignore all others).
+    latest_compute_index = LocationVisit.select(fn.Max(LocationVisit.compute_index)).scalar()
+    visits = (
+        LocationVisit
+        .select()
+        .where(
+            LocationVisit.compute_index == latest_compute_index
+        )
+    )
 
+    for visit in visits:
         yield [[
             visit.compute_index,
             visit.user_id,
@@ -49,4 +59,4 @@ def main(*args, **kwargs):
 
 
 def configure_parser(parser):
-    parser.description = "Dump records of all user visits to locations on the web."
+    parser.description = "Dump most recently computed records of user visits to URLs."

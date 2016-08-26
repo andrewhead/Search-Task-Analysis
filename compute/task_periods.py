bdfe1b090761ebf658a4b69f950c12acd0148861
Andrew Head <head.andrewm@gmail.com>
2016-08-26 01:06:30 -0700

    Compute: Task periods script takes discard and "extras" tuples

--- compute/task_periods.py
+++ compute/task_periods.py
@@ -4,12 +4,92 @@
 from __future__ import unicode_literals
 import logging
 from peewee import fn
+import datetime
 
 from models import QuestionEvent, TaskPeriod
 
 
 logger = logging.getLogger('data')
 CONCERN_COUNT = 6  # needs to be updated to reflect count of concerns in our study
+
+# A tuple of task periods that we want to discard.
+# We discard all task periods for each user-task index pair.
+# We found that there are a number of false detections.
+# This is our way of removing them.
+# At this time, all task periods with that user and task index are discarded, even
+# if only one is incorrect.  If any of the detected task periods are wrongly discarded,
+# you should at them back in with the `EXTRA_TASK_PERIODS` tuple.
+# For additional notes on why these tasks were incorrectly detected and how we corrected them,
+# email <andrewhead@berkeley.edu> for research notes.
+DISCARD_TASK_PERIODS = (
+    {'user_id': 5, 'task_index': 4},
+    {'user_id': 6, 'task_index': 0},
+    {'user_id': 9, 'task_index': 0},
+    {'user_id': 11, 'task_index': 2},
+    {'user_id': 13, 'task_index': 6},
+    {'user_id': 15, 'task_index': 0},
+    {'user_id': 16, 'task_index': 0},
+    {'user_id': 16, 'task_index': 1},
+    {'user_id': 16, 'task_index': 3},
+    {'user_id': 17, 'task_index': 1},
+    {'user_id': 17, 'task_index': 2},
+    {'user_id': 20, 'task_index': 0},
+)
+
+# These task periods will be added in after all task periods have been detected
+# and those matching the discard patterns have been discarded.
+# A task period is defined by a user ID, a task index, and a start and end time
+EXTRA_TASK_PERIODS = ({
+    'user_id': 5,
+    'task_index': 4,
+    'start': datetime.datetime(2016, 6, 20, 14, 31, 58),
+    'end': datetime.datetime(2016, 6, 20, 14, 36, 35),
+}, {
+    'user_id': 6,
+    'task_index': 0,
+    'start': datetime.datetime(2016, 6, 20, 15, 46, 16),
+    'end': datetime.datetime(2016, 6, 20, 15, 52, 49),
+}, {
+    'user_id': 9,
+    'task_index': 0,
+    'start': datetime.datetime(2016, 6, 22, 18, 18, 22),
+    'end': datetime.datetime(2016, 6, 22, 18, 24, 40),
+}, {
+    'user_id': 11,
+    'task_index': 2,
+    'start': datetime.datetime(2016, 6, 24, 9, 47, 24),
+    'end': datetime.datetime(2016, 6, 24, 9, 52, 20),
+}, {
+    'user_id': 13,
+    'task_index': 6,
+    'start': datetime.datetime(2016, 6, 29, 11, 10, 49),
+    'end': datetime.datetime(2016, 6, 29, 11, 11, 59),
+}, {
+    'user_id': 15,
+    'task_index': 0,
+    'start': datetime.datetime(2016, 7, 1, 16, 30, 40),
+    'end': datetime.datetime(2016, 7, 1, 16, 39, 13),
+}, {
+    'user_id': 16,
+    'task_index': 0,
+    'start': datetime.datetime(2016, 7, 6, 18, 21, 13),
+    'end': datetime.datetime(2016, 7, 6, 18, 28, 4),
+}, {
+    'user_id': 16,
+    'task_index': 1,
+    'start': datetime.datetime(2016, 7, 6, 18, 33, 1),
+    'end': datetime.datetime(2016, 7, 6, 18, 43, 30),
+}, {
+    'user_id': 16,
+    'task_index': 3,
+    'start': datetime.datetime(2016, 7, 6, 19, 3, 35),
+    'end': datetime.datetime(2016, 7, 6, 19, 9, 53),
+}, {
+    'user_id': 20,
+    'task_index': 0,
+    'start': datetime.datetime(2016, 7, 13, 16, 19, 44),
+    'end': datetime.datetime(2016, 7, 13, 16, 26, 28),
+})
 
 
 def _get_concern_index(user_id, task_index):
@@ -27,14 +107,14 @@ def _get_concern_index(user_id, task_index):
     return (offset + task_index) % CONCERN_COUNT
 
 
-def compute_task_periods():
+def compute_task_periods(discard_periods=DISCARD_TASK_PERIODS, extra_periods=EXTRA_TASK_PERIODS):
 
     # Create a new index for this computation
     last_compute_index = TaskPeriod.select(fn.Max(TaskPeriod.compute_index)).scalar() or 0
     compute_index = last_compute_index + 1
 
     # Compute the ID of the last user to complete the study
-    max_user_id = QuestionEvent.select(fn.Max(QuestionEvent.user_id)).scalar()
+    max_user_id = QuestionEvent.select(fn.Max(QuestionEvent.user_id)).scalar() or 0
 
     # Compute the time that each user spends in each question
     for user_id in range(0, max_user_id + 1):
@@ -61,19 +141,38 @@ def compute_task_periods():
                     # Save an event if the index of task for a 'post' event that comes
                     # after a task starts matches the task index of the event that started it.
                     if question_event.question_index == start_task_event.question_index:
-                        TaskPeriod.create(
-                            compute_index=compute_index,
-                            user_id=user_id,
-                            task_index=question_event.question_index,
-                            concern_index=_get_concern_index(
-                                user_id, question_event.question_index),
-                            start=start_task_event.time,
-                            end=question_event.time,
-                        )
+
+                        # Only save a task period if its user and index are not in the discard list.
+                        task_discard_specification = {
+                            'user_id': user_id,
+                            'task_index': question_event.question_index,
+                        }
+                        if task_discard_specification not in discard_periods:
+                            TaskPeriod.create(
+                                compute_index=compute_index,
+                                user_id=user_id,
+                                task_index=question_event.question_index,
+                                concern_index=_get_concern_index(
+                                    user_id, question_event.question_index),
+                                start=start_task_event.time,
+                                end=question_event.time,
+                            )
 
                 # As long as we have seen an event for the end of a task, reset
                 # state such that no "start task" event has been seen
                 start_task_event = None
+
+    # The caller may have provided a list of extra task periods to append to the computed results.
+    # Add these records in one by one.
+    for period_data in extra_periods:
+        TaskPeriod.create(
+            compute_index=compute_index,
+            user_id=period_data['user_id'],
+            task_index=period_data['task_index'],
+            concern_index=_get_concern_index(period_data['user_id'], period_data['task_index']),
+            start=period_data['start'],
+            end=period_data['end'],
+        )
 
 
 def main(*args, **kwargs):
--- tests/compute/test_compute_task_periods.py
+++ tests/compute/test_compute_task_periods.py
@@ -36,7 +36,7 @@ class ComputeTaskPeriodTest(TestCase):
 
         # Test: make sure a task has been created with the time bounds of the
         # events that started and ended it.
-        compute_task_periods()
+        compute_task_periods(extra_periods=())
         task_periods = TaskPeriod.select()
         self.assertEqual(task_periods.count(), 1)
         period = task_periods[0]
@@ -54,7 +54,7 @@ class ComputeTaskPeriodTest(TestCase):
             event_type='post task'
         )
 
-        compute_task_periods()
+        compute_task_periods(extra_periods=())
         task_periods = TaskPeriod.select()
         self.assertEqual(task_periods.count(), 0)
 
@@ -68,7 +68,7 @@ class ComputeTaskPeriodTest(TestCase):
             event_type='post task'
         )
 
-        compute_task_periods()
+        compute_task_periods(extra_periods=())
         task_periods = TaskPeriod.select()
         self.assertEqual(task_periods.count(), 0)
 
@@ -92,12 +92,98 @@ class ComputeTaskPeriodTest(TestCase):
             event_type='post task'
         )
 
-        compute_task_periods()
+        compute_task_periods(extra_periods=())
         task_periods = TaskPeriod.select()
         self.assertEqual(task_periods.count(), 2)
         user_ids = [task_period.user_id for task_period in TaskPeriod.select()]
         self.assertIn(0, user_ids)
         self.assertIn(1, user_ids)
+
+    def test_discard_periods_matching_discard_pattern(self):
+
+        START_TIME = datetime.datetime(2000, 1, 1, 12, 0, 1, 0)
+        create_question_event(
+            user_id=3,
+            question_index=4,
+            time=START_TIME,
+            event_type='get task'
+        )
+        create_question_event(
+            user_id=3,
+            question_index=4,
+            time=START_TIME + datetime.timedelta(seconds=1),
+            event_type='post task'
+        )
+        compute_task_periods(
+            discard_periods=({'user_id': 3, 'task_index': 4},),
+            extra_periods=(),
+        )
+        task_periods = TaskPeriod.select()
+        self.assertEqual(task_periods.count(), 0)
+
+    def test_dont_discard_periods_that_dont_match_discard_pattern(self):
+
+        START_TIME = datetime.datetime(2000, 1, 1, 12, 0, 1, 0)
+        create_question_event(
+            user_id=3,
+            question_index=5,
+            time=START_TIME,
+            event_type='get task'
+        )
+        create_question_event(
+            user_id=3,
+            question_index=5,
+            time=START_TIME + datetime.timedelta(seconds=1),
+            event_type='post task'
+        )
+        compute_task_periods(
+            discard_periods=({'user_id': 3, 'question_index': 4},),
+            extra_periods=(),
+        )
+        task_periods = TaskPeriod.select()
+        self.assertEqual(task_periods.count(), 1)
+
+    def test_add_periods_with_extras_specification(self):
+
+        compute_task_periods(extra_periods=({
+            'user_id': 3,
+            'task_index': 4,
+            'start': datetime.datetime(2000, 1, 1, 12, 0, 1, 0),
+            'end': datetime.datetime(2000, 1, 1, 12, 0, 2, 0),
+        },))
+        task_periods = TaskPeriod.select()
+        self.assertEqual(task_periods.count(), 1)
+        period = task_periods[0]
+        self.assertEqual(period.user_id, 3)
+        self.assertEqual(period.task_index, 4)
+        self.assertEqual(period.start, datetime.datetime(2000, 1, 1, 12, 0, 1, 0))
+        self.assertEqual(period.end, datetime.datetime(2000, 1, 1, 12, 0, 2, 0))
+
+    def test_add_periods_from_extras_even_if_it_matches_discard_pattern(self):
+
+        START_TIME = datetime.datetime(2000, 1, 1, 12, 0, 1, 0)
+        create_question_event(
+            user_id=3,
+            question_index=4,
+            time=START_TIME,
+            event_type='get task'
+        )
+        create_question_event(
+            user_id=3,
+            question_index=4,
+            time=START_TIME + datetime.timedelta(seconds=1),
+            event_type='post task'
+        )
+        compute_task_periods(
+            discard_periods=({'user_id': 3, 'task_index': 4},),
+            extra_periods=({
+                'user_id': 3,
+                'task_index': 4,
+                'start': datetime.datetime(2000, 1, 1, 12, 0, 1, 0),
+                'end': datetime.datetime(2000, 1, 1, 12, 0, 2, 0),
+            },))
+        task_periods = TaskPeriod.select()
+        self.assertEqual(task_periods.count(), 1)
 
 
 class ComputeConcernIndexTest(unittest.TestCase):

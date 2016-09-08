bea74e072b083ac3f10509526d77eac065753696
Andrew Head <head.andrewm@gmail.com>
2016-09-07 21:31:55 -0700

    Dump: Routine for standardizing URLs to unique URLs
    Based on work I've done on computing unique URLs in our Tableau
    workbooks.

--- dump/_urls.py
+++ dump/_urls.py
@@ -1570,6 +1570,103 @@ LABELS = [{
 }]
 
 
+def standardize_url(url):
+    '''
+    Standardize a URL.  This means reducing a URL to single unique URL that will
+    be the same for all other URLs that point to the same content.
+    In most cases, this means just removing query parameters and fragments.
+    Though this behavior gets more complex for other pages.
+    For example, fragments are needed to disambiguate between different forums
+    on Google Groups.  This routine takes care of all of that logic.
+    '''
+
+    url_parsed = urlparse(url)
+    scheme = url_parsed.scheme
+    fragment = url_parsed.fragment
+    query = url_parsed.query
+
+    if scheme == "view-source":
+        # If we are viewing source, the original scheme, domain, and path all get
+        # shoved into the "path" (though the fragment and query remain separate).
+        # Here we split out the domain and path again.
+        page_url = url_parsed.path
+        domain = "view-source:" + urlparse(page_url).netloc
+        path = urlparse(page_url).path
+    else:
+        domain = url_parsed.netloc
+        path = url_parsed.path
+
+    domain_without_www = re.sub("^www\.", "", domain)
+
+    # Preserve the query parameters that identify Panda3D topics and forums.
+    if domain_without_www == "panda3d.org":
+
+        # Note that even though a URL for a Panda3D topic might include both
+        # a topic number and a forum number, a few tests on my part showed that
+        # the forum number isn't actually needed.  The topic numbers
+        # are probably unique and independent of forum number.
+        if path == "/viewtopic.php" and re.search(r"(^|&)t=", query):
+            topic_id = re.search(r"(^|&)t=(\d+)", query).group(2)
+            return domain_without_www + path + "?t=" + topic_id
+        elif path == "/viewforum.php" and re.search(r"(^|&)f=", query):
+            forum_id = re.search(r"(^|&)f=(\d+)", query).group(2)
+            return domain_without_www + path + "?f=" + forum_id
+        elif path == "/showss.php" and re.search(r"(^|&)shot=", query):
+            shot_name = re.search(r"(^|&)shot=([^&]+)", query).group(2)
+            return domain_without_www + path + "?shot=" + shot_name
+
+    elif domain_without_www == "youtube.com" and path == "" and re.search(r"(^|&)v=", query):
+        video_id = re.search(r"(^|&)v=(\w+)", query).group(2)
+        return domain_without_www + "?v=" + video_id
+
+    elif domain_without_www == "groups.google.com":
+
+        # Convert all Google Groups forums and topics into their own URLs
+        if re.search(r"^!topic/", fragment) or re.search(r"^!forum/", fragment):
+            return domain_without_www + path + fragment
+
+        # Coalesce all search anywhere on Google Groups into one URL
+        if re.search(r"^!searchin", fragment):
+            return domain_without_www + path + "!searchin"
+
+    # Tigsource forums and topics all get their own URLs too
+    # We do topics before forums in case topic pages also have a
+    # forum ID as part of their query parameters
+    elif domain_without_www == "forums.tigsource.com":
+        if re.search(r"(^|&)topic=", query):
+            topic_id = re.search(r"(^|&)topic=([0-9.]+)", query).group(2)
+            return domain_without_www + path + "?topic=" + topic_id
+        elif re.search(r"(^|&)board=", query):
+            board_id = re.search(r"(^|&)board=([0-9.]+)", query).group(2)
+            return domain_without_www + path + "?board=" + board_id
+        elif re.search(r"(^|&)action=search2", query):
+            return domain_without_www + path + "?action=search2"
+
+    # r.search.yahoo.com links from this study tended to be redirects.  I expect
+    # that they were encountered as someone clicked on a search result from Yahoo.
+    # Here, we make all redirects just be one single URL.
+    elif domain_without_www == "r.search.yahoo.com" and path.startswith("/_ylt="):
+        return domain_without_www + "/_ylt=redirect"
+
+    # Stack Overflow questions can be referred to by their number alone, and by their number
+    # and a longer readable name, and after that there can be the ID of an answer.
+    # We consider the all to be the same page.
+    elif domain_without_www == "stackoverflow.com" and re.search(r"^/questions/\d+/", path):
+        question_id = re.search(r"^/questions/(\d+)/", path).group(1)
+        return domain_without_www + "/questions/" + question_id
+
+    # If this is the experiment site, return just one domain.  This squashes all visits
+    # to the experiment site into just one URL
+    elif domain_without_www == "searchlogger.tutorons.com":
+        return domain_without_www
+
+    # Squash all browser pages (new tabs, preferences pages, etc.) into one unique URL
+    elif scheme == "about":
+        return "browser_page"
+
+    return domain_without_www + path
+
+
 def get_label(url):
     ''' Look up a descriptive label for what content is at the URL. '''
 
---
+++ tests/dump/test_compute_url_labels.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import logging
+import unittest
+
+from dump._urls import standardize_url
+
+
+logger = logging.getLogger('data')
+
+
+class StandardizeUrlTest(unittest.TestCase):
+
+    def test_by_default_standard_url_is_domain_and_path(self):
+        standardized = standardize_url("http://site.com/path#fragment?q=query")
+        self.assertEqual(standardized, "site.com/path")
+
+    def test_standardization_removes_www_prefix_and_schema(self):
+        standardized = standardize_url("http://www.site.com")
+        self.assertEqual(standardized, "site.com")
+
+    def test_panda3d_topics_keep_topic_id(self):
+        standardized = standardize_url("http://panda3d.org/viewtopic.php?f=1&t=2")
+        self.assertEqual(standardized, "panda3d.org/viewtopic.php?t=2")
+
+    def test_panda3d_forums_keep_forum_id(self):
+        standardized = standardize_url("http://panda3d.org/viewforum.php?f=1")
+        self.assertEqual(standardized, "panda3d.org/viewforum.php?f=1")
+
+    def test_panda3d_screenshots_show_keep_shot_name(self):
+        # We found that some of our experimental data includes slashes in the name
+        # of the shot, so we replicate that here.
+        standardized = standardize_url(
+            "http://panda3d.org/showss.php?shot=path/to/photo&otherparam=1"
+        )
+        self.assertEqual(standardized, "panda3d.org/showss.php?shot=path/to/photo")
+
+    def test_youtube_videos_preserve_v_parameter(self):
+        standardized = standardize_url("http://youtube.com?v=DRR9fOXkfRE")
+        self.assertEqual(standardized, "youtube.com?v=DRR9fOXkfRE")
+
+    def test_google_groups_topic_keep_fragment(self):
+        standardized = standardize_url(
+            "http://groups.google.com/forum/#!topic/keras-users/epFdzcxl8Gg"
+        )
+        self.assertEqual(standardized, "groups.google.com/forum/!topic/keras-users/epFdzcxl8Gg")
+
+    def test_google_groups_forums_keep_fragment(self):
+        standardized = standardize_url("http://groups.google.com/forum/#!forum/keras-users")
+        self.assertEqual(standardized, "groups.google.com/forum/!forum/keras-users")
+
+    def test_google_groups_simplify_search_url(self):
+        standardized = standardize_url(
+            "http://groups.google.com/forum/#!searchin/keras-users/model"
+        )
+        self.assertEqual(standardized, "groups.google.com/forum/!searchin")
+
+    def test_tigsource_forums_keep_topic_id(self):
+        standardized = standardize_url("http://forums.tigsource.com/index.php?topic=100.0")
+        self.assertEqual(standardized, "forums.tigsource.com/index.php?topic=100.0")
+
+    def test_tigsource_forums_keep_board_id(self):
+        standardized = standardize_url("http://forums.tigsource.com/index.php?board=20.0")
+        self.assertEqual(standardized, "forums.tigsource.com/index.php?board=20.0")
+
+    def test_tigsource_forums_preserve_search_query_parameter(self):
+        standardized = standardize_url(
+            "http://forums.tigsource.com/index.php?action=search2;params=PARAMS"
+        )
+        self.assertEqual(standardized, "forums.tigsource.com/index.php?action=search2")
+
+    def test_experiment_site_is_reduced_to_its_domain(self):
+        standardized = standardize_url("http://searchlogger.tutorons.com/long/path#with-fragment")
+        self.assertEqual(standardized, "searchlogger.tutorons.com")
+
+    def test_browser_pages_are_reduced_to_the_words_browser_page(self):
+        standardized = standardize_url("about:preferences")
+        self.assertEqual(standardized, "browser_page")
+
+    def test_yahoo_redirect_link_reduced_to_standard_path(self):
+        standardized = standardize_url("http://r.search.yahoo.com/_ylt=AwrTccsomething")
+        self.assertEqual(standardized, "r.search.yahoo.com/_ylt=redirect")
+
+    def test_stack_overflow_question_reduce_to_id_as_path(self):
+        standardized = standardize_url("http://stackoverflow.com/questions/1000/long-name")
+        self.assertEqual(standardized, "stackoverflow.com/questions/1000")
+
+    def test_pages_viewed_as_source_gets_view_source_prepended(self):
+        standardized = standardize_url("view-source:http://site.com")
+        self.assertEqual(standardized, "view-source:site.com")

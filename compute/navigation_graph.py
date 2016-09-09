186a7bb8d4a1d5836fb46744d532ae63fb3cdbc1
Andrew Head <head.andrewm@gmail.com>
2016-09-08 20:03:41 -0700

    Compute: Graph gets "Start" and "End" nodes, ignores redirects

--- compute/navigation_graph.py
+++ compute/navigation_graph.py
@@ -7,20 +7,39 @@ import itertools
 from peewee import fn
 
 from models import LocationVisit, NavigationVertex, NavigationEdge
-from dump._urls import get_label
 
 
 logger = logging.getLogger('data')
 
 
-def compute_navigation_graph(concern_index=None, label_function=get_label):
+class Vertex(object):
     '''
-    `label_function` must take a URL as its argument and return a dictionary with these properties:
-    * `name`: the name of the page type (string)
-    * `project`: whether this URL belongs to the project's documentation (Boolean)
-    * `target`: if this is a search page, what is its target? (string or None)
-    * `domain`: the domain name of the URL (string)
+    A vertex in a graph of web navigation behavior.  Includes data of how often it was
+    visited, and how much time was spent their.
     '''
+    def __init__(self, page_type, occurrences=0, total_time=0, mean_time=0):
+        self.page_type = page_type
+        self.occurrences = occurrences
+        self.total_time = total_time
+        self.mean_time = mean_time
+
+
+class Edge(object):
+    '''
+    An edge in a graph of web navigation behavior.  Includes data of how often this
+    edge was taken between these two vertices, and the probability that it was taken
+    relative to all edges from the source vertex.  This data has to be manually
+    computed and inserted into this object.
+    '''
+    def __init__(self, source_vertex, target_vertex, occurrences=0, probability=0):
+        self.source_vertex = source_vertex
+        self.target_vertex = target_vertex
+        self.occurrences = 0
+        self.probability = probability
+
+
+def compute_navigation_graph(page_type_lookup, concern_index=None):
+
     # Create a new index for this computation
     last_compute_index = NavigationVertex.select(
         fn.Max(NavigationVertex.compute_index)
@@ -40,61 +59,77 @@ def compute_navigation_graph(concern_index=None, label_function=get_label):
     participant_ids = set([visit.user_id for visit in visits])
     concern_indexes = set([visit.concern_index for visit in visits])
 
-    vertices = {}
+    # The list of vertices needs to be populated with a start and end node.
+    # All navigation behavior starts at the "Start" node, and ends at the "End" node
+    vertices = {
+        "Start": Vertex("Start", occurrences=1),
+        "End": Vertex("End", occurrences=1),
+    }
     edges = {}
-    last_page_type = None
+    last_vertex = vertices["Start"]
 
     # Go through every concern for every participant.  For each page they visit,
-    # increment the visits to a vertex.  For each transition from one page to the next,
-    # increment the occurrence of a transition between two page types.
+    # increment the visits to the corresponding vertex.  For each transition from one
+    # page to the next, increment the occurrence of a transition between two page types.
     for participant_id in participant_ids:
         for concern_index in concern_indexes:
 
             participant_concern_visits = visits.where(
                 LocationVisit.user_id == participant_id,
                 LocationVisit.concern_index == concern_index,
-            )
+            ).order_by(LocationVisit.start.asc())
 
             for visit in participant_concern_visits:
 
                 # Get the type of the page visited
-                page_label = label_function(visit.url)
-                page_type = page_label['name'] if page_label is not None else 'Unclassified'
+                if visit.url in page_type_lookup:
+                    url_info = page_type_lookup[visit.url]
+                    page_type = url_info['main_type']
+                    # If this is a redirect, then just skip it.  It's more important
+                    # to link the URL before it to the link the redirect points to.
+                    if url_info['redirect']:
+                        continue
+                else:
+                    logger.warn(
+                        "URL %s not in page type lookup.  Giving it 'Unknown' type", visit.url
+                    )
+                    page_type = "Unknown"
 
                 # Add a new vertex for this page type if it doesn't exist
                 if page_type not in vertices:
-                    vertices[page_type] = {
-                        'occurrences': 0,
-                        'total_time': 0,
-                    }
+                    vertices[page_type] = Vertex(page_type)
 
                 # Save that we have seen this page type one more time
-                vertices[page_type]['occurrences'] += 1
+                vertex = vertices[page_type]
+                vertex.occurrences += 1
 
                 # Add the time spent to the total time spent for this page type
                 time_passed = visit.end - visit.start
                 seconds = time_passed.seconds + (time_passed.microseconds / float(1000000))
-                vertices[page_type]['total_time'] += seconds
+                vertex.total_time += seconds
 
                 # Connect an edge between the last page visited and this one
-                if last_page_type is not None:
-                    if (last_page_type, page_type) not in edges:
-                        edges[(last_page_type, page_type)] = {
-                            'occurrences': 0,
-                        }
-                    edge_properties = edges[(last_page_type, page_type)]
-                    edge_properties['occurrences'] += 1
+                if (last_vertex.page_type, vertex.page_type) not in edges:
+                    edges[(last_vertex.page_type, vertex.page_type)] = Edge(last_vertex, vertex)
+                edge = edges[(last_vertex.page_type, vertex.page_type)]
+                edge.occurrences += 1
 
                 # Redefine the last page so we know in the next iteration what was just visited.
-                last_page_type = page_type
+                last_vertex = vertex
 
-            # After each participant or each concern, we reset the last_page_type
-            # to `None` so that we only connect edges from a single task.
-            last_page_type = None
+            # After each participant or each concern, connect from the last URL to the end vertex
+            end_vertex = vertices['End']
+            if (last_vertex.page_type, end_vertex.page_type) not in edges:
+                edges[(last_vertex.page_type, end_vertex.page_type)] = Edge(last_vertex, end_vertex)
+            edge = edges[(last_vertex.page_type, end_vertex.page_type)]
+            edge.occurrences += 1
+
+            # After each participant or each concern, we reset the last_page_type to "Start"
+            last_vertex = vertices['Start']
 
     # Compute the mean time spent on each vertex
-    for vertex, properties in vertices.items():
-        properties['mean_time'] = properties['total_time'] / properties['occurrences']
+    for vertex in vertices.values():
+        vertex.mean_time = vertex.total_time / float(vertex.occurrences)
 
     # Compute the transition probability for each edge leaving a vertex.
     # First, group all edges by their source vertex
@@ -110,32 +145,32 @@ def compute_navigation_graph(concern_index=None, label_function=get_label):
         group_edges = dict(filter(lambda (k, v): k in edge_keys, edges.items()))
 
         # Compute the probability of each edge being taken
-        total_occurrences = sum([edge['occurrences'] for edge in group_edges.values()])
-        for edge_key, properties in group_edges.items():
-            properties['probability'] = float(properties['occurrences']) / total_occurrences
+        total_occurrences = sum([e.occurrences for e in group_edges.values()])
+        for edge in group_edges.values():
+            edge.probability = float(edge.occurrences) / total_occurrences
 
     # Save all vertices to the database
     vertex_models = {}
-    for page_type, properties in vertices.items():
+    for vertex in vertices.values():
         vertex_model = NavigationVertex.create(
             compute_index=compute_index,
-            page_type=page_type,
-            occurrences=properties['occurrences'],
-            total_time=properties['total_time'],
-            mean_time=properties['mean_time'],
+            page_type=vertex.page_type,
+            occurrences=vertex.occurrences,
+            total_time=vertex.total_time,
+            mean_time=vertex.mean_time,
         )
         # We store a dictionary from page type to vertex model so
         # we can look up these models when saving the edges.
-        vertex_models[page_type] = vertex_model
+        vertex_models[vertex.page_type] = vertex_model
 
     # Save all edges to the database
-    for (source_page_type, target_page_type), properties in edges.items():
+    for edge in edges.values():
         NavigationEdge.create(
             compute_index=compute_index,
-            source_vertex=vertex_models[source_page_type],
-            target_vertex=vertex_models[target_page_type],
-            occurrences=properties['occurrences'],
-            probability=properties['probability'],
+            source_vertex=vertex_models[edge.source_vertex.page_type],
+            target_vertex=vertex_models[edge.target_vertex.page_type],
+            occurrences=edge.occurrences,
+            probability=edge.probability,
         )
 
 
@@ -146,6 +181,15 @@ def main(concern_index, *args, **kwargs):
 def configure_parser(parser):
     parser.description = "Compute the vertices and edges of a graph that shows the types " +\
         "of pages that participants dwell on and how they get between them."
+    parser.add_argument(
+        "page_types_json_filename",
+        help=(
+            "Name of a JSON file that maps URLs to file types.  " +
+            "The format of each row should be:" +
+            "\"<url>\": {\"main_type\": \"<main type>\", \"types\": " +
+            "[<list of all relevant types>]}"
+        )
+    )
     parser.add_argument(
         "--concern-index",
         help=(
--- tests/compute/test_compute_navigation_graph.py
+++ tests/compute/test_compute_navigation_graph.py
@@ -12,14 +12,11 @@ from models import LocationVisit, NavigationVertex, NavigationEdge
 
 
 logger = logging.getLogger('data')
-
-
-def label_url(url):
-    if url == 'page1':
-        page_type = "page_type_1"
-    elif url == 'page2':
-        page_type = "page_type_2"
-    return {'name': page_type}
+PAGE_TYPE_LOOKUP = {
+    "page1": {"main_type": "page_type_1", "redirect": False},
+    "page2": {"main_type": "page_type_2", "redirect": False},
+    "redirect": {"main_type": "redirect", "redirect": True},
+}
 
 
 class ComputeLocationVisitsTest(TestCase):
@@ -49,19 +46,25 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 6, 0),
         )
 
-        # Compute a navigation graph by inspecting the location visits one by one
-        # We provide a "dummy" function that will assign a page type to each URL
-        # that we can keep distinct from the typical page label function that's
-        # likely to change in the near future.
-        compute_navigation_graph(label_function=label_url)
+        # Compute a navigation graph by inspecting the location visits one by one.
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         vertices = NavigationVertex.select()
 
-        # Two vertices have been created, one for each page type
-        self.assertEqual(vertices.count(), 2)
+        # Four vertices have been created:
+        # * two, for the two distinct URLs above
+        # * one for the "Start" vertex
+        # * one for the "End" vertex
+        self.assertEqual(vertices.count(), 4)
         page_type_1_vertex = vertices.where(NavigationVertex.page_type == "page_type_1").first()
         page_type_2_vertex = vertices.where(NavigationVertex.page_type == "page_type_2").first()
         self.assertEqual(page_type_1_vertex.occurrences, 2)
         self.assertEqual(page_type_2_vertex.occurrences, 1)
+
+        # Start and end vertices should have 1 occurrence by default
+        start_vertex = vertices.where(NavigationVertex.page_type == "Start").first()
+        end_vertex = vertices.where(NavigationVertex.page_type == "End").first()
+        self.assertEqual(start_vertex.occurrences, 1)
+        self.assertEqual(end_vertex.occurrences, 1)
 
     def test_vertex_total_time_counts_time_of_all_visits(self):
 
@@ -81,7 +84,7 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 6, 0),  # 1 second
         )
 
-        compute_navigation_graph(label_function=label_url)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         vertices = NavigationVertex.select()
         page_type_1_vertex = vertices.where(NavigationVertex.page_type == "page_type_1").first()
         page_type_2_vertex = vertices.where(NavigationVertex.page_type == "page_type_2").first()
@@ -99,7 +102,7 @@ class ComputeLocationVisitsTest(TestCase):
         create_location_visit(
             url="page1",
             start=datetime.datetime(2000, 1, 1, 12, 0, 3, 0),
-            end=datetime.datetime(2000, 1, 1, 12, 0, 6, 0),  # + 3 seconds = 4 seconds (avg: 2s)
+            end=datetime.datetime(2000, 1, 1, 12, 0, 5, 0),  # + 2 seconds = 3 seconds (avg: 1.5s)
         )
         create_location_visit(
             url="page2",
@@ -107,12 +110,12 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 6, 0),  # 1 second (avg: 1s)
         )
 
-        compute_navigation_graph(label_function=label_url)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         vertices = NavigationVertex.select()
         page_type_1_vertex = vertices.where(NavigationVertex.page_type == "page_type_1").first()
         page_type_2_vertex = vertices.where(NavigationVertex.page_type == "page_type_2").first()
 
-        self.assertEqual(page_type_1_vertex.mean_time, 2)
+        self.assertEqual(page_type_1_vertex.mean_time, 1.5)
         self.assertEqual(page_type_2_vertex.mean_time, 1)
 
     def test_edge_added_between_all_consecutive_visits(self):
@@ -133,16 +136,23 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 6, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         edges = NavigationEdge.select()
-        self.assertEqual(edges.count(), 2)
+
+        # There will be 4 edges:
+        # * 2 for the transitions between the 3 URLs above
+        # * 1 for the transition from "Start" to the first URL
+        # * 1 for the transition from the last URL to "End"
+        self.assertEqual(edges.count(), 4)
 
         edge_page_type_pairs = [
             (edge.source_vertex.page_type, edge.target_vertex.page_type)
             for edge in edges
         ]
+        self.assertIn(("Start", "page_type_1"), edge_page_type_pairs)
         self.assertIn(("page_type_1", "page_type_1"), edge_page_type_pairs)
         self.assertIn(("page_type_1", "page_type_2"), edge_page_type_pairs)
+        self.assertIn(("page_type_2", "End"), edge_page_type_pairs)
 
     def test_edge_occurrences_counts_number_of_transitions_between_page_types(self):
 
@@ -167,14 +177,16 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 8, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         edges = NavigationEdge.select()
         edge_dict = {
             (edge.source_vertex.page_type, edge.target_vertex.page_type): edge
             for edge in edges
         }
+        self.assertEqual(edge_dict[('Start', 'page_type_1')].occurrences, 1)
         self.assertEqual(edge_dict[('page_type_1', 'page_type_1')].occurrences, 2)
         self.assertEqual(edge_dict[('page_type_1', 'page_type_2')].occurrences, 1)
+        self.assertEqual(edge_dict[('page_type_2', 'End')].occurrences, 1)
 
     def test_edge_transition_probabilities_normalize_occurrences(self):
 
@@ -204,14 +216,15 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 10, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
         edges = NavigationEdge.select()
         edge_dict = {
             (edge.source_vertex.page_type, edge.target_vertex.page_type): edge
             for edge in edges
         }
-        self.assertAlmostEqual(edge_dict[('page_type_1', 'page_type_1')].probability, float(2) / 3)
-        self.assertAlmostEqual(edge_dict[('page_type_1', 'page_type_2')].probability, float(1) / 3)
+        self.assertAlmostEqual(edge_dict[('page_type_1', 'page_type_1')].probability, float(1) / 2)
+        self.assertAlmostEqual(edge_dict[('page_type_1', 'page_type_2')].probability, float(1) / 4)
+        self.assertAlmostEqual(edge_dict[('page_type_1', 'End')].probability, float(1) / 4)
         self.assertAlmostEqual(edge_dict[('page_type_2', 'page_type_1')].probability, 1)
 
     def test_edge_not_added_between_participants(self):
@@ -224,13 +237,16 @@ class ComputeLocationVisitsTest(TestCase):
         )
         create_location_visit(
             user_id=1,
-            url="page1",
+            url="page2",
             start=datetime.datetime(2000, 1, 1, 12, 0, 3, 0),
             end=datetime.datetime(2000, 1, 1, 12, 0, 4, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
-        self.assertEqual(NavigationEdge.select().count(), 0)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
+
+        # 4 edges should have been created---between the Start vertex, the one URL, and
+        # the End vertex for each of the participants
+        self.assertEqual(NavigationEdge.select().count(), 4)
 
     def test_edge_not_added_between_concerns_for_the_same_participant(self):
 
@@ -242,13 +258,16 @@ class ComputeLocationVisitsTest(TestCase):
         )
         create_location_visit(
             concern_index=1,
-            url="page1",
+            url="page2",
             start=datetime.datetime(2000, 1, 1, 12, 0, 3, 0),
             end=datetime.datetime(2000, 1, 1, 12, 0, 4, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
-        self.assertEqual(NavigationEdge.select().count(), 0)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
+
+        # 4 edges should have been created---between the Start vertex, the one URL, and
+        # the End vertex for each of the concerns
+        self.assertEqual(NavigationEdge.select().count(), 4)
 
     def test_graph_computation_uses_only_latest_computed_visits(self):
 
@@ -271,11 +290,11 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 4, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
-        self.assertEqual(NavigationEdge.select().count(), 1)
-        edge = NavigationEdge.select().first()
-        self.assertEqual(edge.source_vertex.page_type, "page_type_2")
-        self.assertEqual(edge.target_vertex.page_type, "page_type_2")
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
+        self.assertEqual(NavigationEdge.select().count(), 3)
+        edges = NavigationEdge.select()
+        transition_list = [(e.source_vertex.page_type, e.target_vertex.page_type) for e in edges]
+        self.assertIn(("page_type_2", "page_type_2"), transition_list)
 
     def test_filter_to_only_one_concern_if_concern_index_provided(self):
 
@@ -306,11 +325,11 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 8, 0),
         )
 
-        compute_navigation_graph(concern_index=1, label_function=label_url)
-        self.assertEqual(NavigationEdge.select().count(), 1)
-        edge = NavigationEdge.select().first()
-        self.assertEqual(edge.source_vertex.page_type, "page_type_2")
-        self.assertEqual(edge.target_vertex.page_type, "page_type_2")
+        compute_navigation_graph(concern_index=1, page_type_lookup=PAGE_TYPE_LOOKUP)
+        self.assertEqual(NavigationEdge.select().count(), 3)
+        edges = NavigationEdge.select()
+        transition_list = [(e.source_vertex.page_type, e.target_vertex.page_type) for e in edges]
+        self.assertIn(("page_type_2", "page_type_2"), transition_list)
 
     def test_include_all_concerns_if_no_concern_index_provided(self):
 
@@ -340,5 +359,21 @@ class ComputeLocationVisitsTest(TestCase):
             end=datetime.datetime(2000, 1, 1, 12, 0, 8, 0),
         )
 
-        compute_navigation_graph(label_function=label_url)
-        self.assertEqual(NavigationEdge.select().count(), 2)
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
+        self.assertEqual(NavigationEdge.select().count(), 6)
+
+    def test_graph_skips_redirects(self):
+
+        # Because redirects typically don't show any content but are just a gateway to
+        # another page, we will leave them out of the graph of navigation.  It's more
+        # meaningful to connect the link before it, and the link that it points to.
+        create_location_visit(
+            url="redirect",
+            start=datetime.datetime(2000, 1, 1, 12, 0, 1, 0),
+            end=datetime.datetime(2000, 1, 1, 12, 0, 2, 0),
+        )
+
+        compute_navigation_graph(page_type_lookup=PAGE_TYPE_LOOKUP)
+
+        # There should only be one edge---from "Start" to "End"
+        self.assertEqual(NavigationEdge.select().count(), 1)
